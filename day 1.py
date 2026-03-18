a8ffa929a1ef2572fc655dcf6e310e98a01425ed
vidyanjaligundubogula <vidyanjaligundubogula6@gmail.com>
2026-03-18 17:40:54 +0530

    day 1

---
+++ day 1.py
@@ -0,0 +1,1 @@
+print ("iam vidhyanjali ,i joined in codegnan")
---
+++ my details.py
@@ -0,0 +1,5 @@
+print("my name is vidhyanjali")
+print("i am from Andhra univesity")
+print("2025 passedout with 7.83 CGPA")
+print("---------")
+ 
