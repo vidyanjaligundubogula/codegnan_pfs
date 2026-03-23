04e2a6a2d05c9a5a109ab16881afee8900be13d4
vidyanjaligundubogula <vidyanjaligundubogula6@gmail.com>
2026-03-23 13:15:36 +0530

    day 4 Python programming

---
+++ day 3.py
@@ -0,0 +1,71 @@
+''' if statement -- (if statement helps is used to check any condition if the condition become true then it will enter inside the(if ststement)
+
+
+
+x = 10
+y = 33
+if x==y:
+    print(True)
+else:
+    print(False)
+    
+
+age = int(input("Enter your age"))
+if age >= 18:
+    print("you are an adult")
+    print("you are eligible")
+
+
+
+  #if-else statement -- this also called as fall back statement which only execute when the (if statement) becomes false 
+
+x = int(input("Enter the age :"))
+y = 50
+if x<=y:
+    print("true x is younger than y")
+else:
+    print("false x is not younger than y")
+    
+   
+  
+   
+codegnan_eligibility = int(input("Enter the passout year"))
+y = 8.5
+if codegnan_eligibility <= y:
+    print("Not Eligible")
+else:
+    print("Eligible")
+
+#if elif -else statement (if + else) --- in the elif part, i can check the condition as backup for more conditions '''
+ 
+''' students_marks- int(input("Enter your marks : "))
+if student_marks > 90:
+    print("You got A+ grade")
+elif student_marks > 80:
+    print("You got B+ grade")
+elif students_marks < 80:
+    print("You got C+ grade")
+else:
+    print("you are fail") '''
+
+
+num1 = float(input("Enter first number"))
+num2 = float(input("Enter second number"))
+
+operation = input("Enter operation (+,-,*,/): ")
+
+if operation == "+":
+    print("Result:",num1 + num2)
+
+elif operation == "-":
+    print("Result:",num1 - num2)
+
+elif operation == "*":
+    print("Result:",num1 * num2)
+
+elif operation == "/":
+    print("Result:",num1 / num2)
+
+
+
+
